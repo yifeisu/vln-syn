5ff8e08f7f45e80878af00f24717202404c43148
yifeisu <suyifei2022@ia.ac.cn>
2022-04-19 14:29:17 +0800

    fix the bash file

--- pretrain_src/run/0-lxmert_3p_5m_512_128_5x.bash
+++ pretrain_src/run/0-lxmert_3p_5m_512_128_5x.bash
@@ -1,9 +1,9 @@
-name=pretrain_3p_5m_2048_128
+name=pretrain_3p_5m_512_128_5x
 
 args="--gpu_id 0
       --name ${name}
 
-      --epoch 4
+      --epoch 3
       --batchSize 64
       --num_workers 12
 
@@ -14,4 +14,4 @@ args="--gpu_id 0
       --img_feat_dim 512
       --angle_feat_dim 128"
 
-nohup python -u -m torch.distributed.launch --nproc_per_node=1 main_r2r_3p_ddp.py $args > vln-pretrain.log 2>&1 &
+nohup python -u -m torch.distributed.launch --nproc_per_node=1 main_r2r_3p_ddp.py $args > ${name}.log 2>&1 &
--- pretrain_src/run/1-lxmert_3p_5m_2048_128_5x.bash
+++ pretrain_src/run/1-lxmert_3p_5m_2048_128_5x.bash
@@ -1,16 +1,17 @@
-gpu_id=0
+name=pretrain_3p_5m_2018_128_5x
 
-args="--gpu_id ${gpu_id}
-      --name pretrain_3p_5m_2048_128
+args="--gpu_id 0
+      --name ${name}
 
-      --epoch 4
-      --batchSize 128
+      --epoch 3
+      --batchSize 64
       --num_workers 12
 
-      --feature res152_imagenet
+      --x_layers 5
+      --proxy mlm,nap,tom
 
+      --feature res152_imagenet
       --img_feat_dim 2048
       --angle_feat_dim 128"
 
-#nohup python -u -m torch.distributed.launch --nproc_per_node=1 main_r2r_3p_ddp.py $args > vln-pretrain.log 2>&1 &
-python -u -m torch.distributed.launch --nproc_per_node=1 main_r2r_3p_ddp.py $args
+nohup python -u -m torch.distributed.launch --nproc_per_node=1 main_r2r_3p_ddp.py $args > ${name}.log 2>&1 &

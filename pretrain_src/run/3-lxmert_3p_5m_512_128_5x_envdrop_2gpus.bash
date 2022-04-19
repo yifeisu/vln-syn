3666c8d3a831907093b9106b398c38b713cee4e9
yifeisu <suyifei2022@ia.ac.cn>
2022-04-19 22:23:51 +0800

    adjust the 3p task

--- pretrain_src/run/3-lxmert_3p_5m_512_128_5x_envdrop_2gpus.bash
+++ pretrain_src/run/3-lxmert_3p_5m_512_128_5x_envdrop_2gpus.bash
@@ -1,6 +1,6 @@
 name=pretrain_3p_5m_512_128_5x_envdrop_2gpus
 
-args="--gpu_id 5,6,7
+args="--gpu_id 2,3
       --name ${name}
 
       --epoch 4
@@ -14,4 +14,4 @@ args="--gpu_id 5,6,7
       --img_feat_dim 512
       --angle_feat_dim 128"
 
-nohup python -u -m torch.distributed.launch --nproc_per_node=3 main_r2r_ddp.py $args > ${name}.log 2>&1 &
+nohup python -u -m torch.distributed.launch --nproc_per_node=2 main_r2r_ddp.py $args > ${name}.log 2>&1 &

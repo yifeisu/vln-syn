a07def739841420a0b20fe01ae20d1abadc6ab43
yifeisu <suyifei2022@ia.ac.cn>
2022-04-22 20:26:58 +0800

    fix a bug

--- finetune_src/run/rvb_train_agent.bash
+++ finetune_src/run/rvb_train_agent.bash
@@ -22,4 +22,4 @@ flag="--vlnbert prevalent
       --dropout 0.5"
 
 mkdir -p snap/$name
-CUDA_VISIBLE_DEVICES=1 python r2r_src/train.py $flag --name $name
+CUDA_VISIBLE_DEVICES=0 python r2r_src/train.py $flag --name $name

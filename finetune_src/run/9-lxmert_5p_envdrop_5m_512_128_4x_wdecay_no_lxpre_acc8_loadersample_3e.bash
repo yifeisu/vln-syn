1e2da4140bf390a9afd369e94709a4fc9898290d
yifeisu <suyifei2022@ia.ac.cn>
2022-04-23 23:47:04 +0800

    add 2 finetune task to check whether the nar is useful

--- finetune_src/run/9-lxmert_5p_envdrop_5m_512_128_4x_wdecay_no_lxpre_acc8_loadersample_3e.bash
+++ finetune_src/run/9-lxmert_5p_envdrop_5m_512_128_4x_wdecay_no_lxpre_acc8_loadersample_3e.bash
@@ -1,7 +1,7 @@
 # name of the exp, where the "log.txt" and "weights" store
-name=vln_lxmert_5p_envdrop_5m_512_128_4x_wdecay_no_lxpre_acc8_loadersample_deletecliptok_2eb
+name=vln_lxmert_5p_envdrop_5m_512_128_4x_wdecay_no_lxpre_acc8_loadersample_deletecliptok_3eb
 
-args="--gpu_id 2
+args="--gpu_id 3
       --name ${name}
       --train auglistener
 
@@ -9,7 +9,7 @@ args="--gpu_id 2
       --batch_size 16
       --optim adamW
 
-      --pretrain_path ../pretrain_src/snap/pretrain_5p_5m_512_128_4x_envdrop_3gpus_acc8_loader_sample/best_model_1/bert/
+      --pretrain_path ../pretrain_src/snap/pretrain_5p_5m_512_128_4x_envdrop_3gpus_acc8_loader_sample/2/bert/
 
       --features clip_vit
       --aug r2r_data/prevalent_aug.json

582fbf70721cc45ba860ed0ba973147e20f24e4b
yifeisu <suyifei2022@ia.ac.cn>
2022-04-30 22:58:15 +0800

    add a new finrtune task for contrast

---
+++ finetune_src/run/3-lxmert_4x_5p_places_stop_candidate_acc4_wdecay_dec_hamt_6e.bash
@@ -0,0 +1,29 @@
+# name of the exp, where the "log.txt" and "weights" store
+name=${0}
+name=${name%.*}
+name=${name#*/}
+
+args="--gpu_id ${1}
+      --name ${name}
+      --train auglistener
+
+      --epoch 4
+      --batch_size 8
+      --optim adamW
+
+      --pretrain_path ../pretrain_src/snap/0-lxmert_4x_scratch_5p_places_envdrop_candidate_acc4/5/bert
+
+      --features res152-places365
+      --aug r2r_data/prevalent_aug.json
+      --speaker_aug 0
+      
+	  --decision_mode hamt
+      --x_layers 4
+      --feature_size 2048
+      --angleFeatSize 128
+
+      --decay 0.0000001
+      --feedback sample
+      --mlWeight 0.20"
+
+nohup python -u r2r_src_simv1/train.py $args  > ${name}.log 2>&1 &

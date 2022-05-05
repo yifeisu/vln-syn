ceff46d593389002d21486647326babc39923fe3
yifeisu <suyifei2022@ia.ac.cn>
2022-05-05 23:24:00 +0800

    add 2 finetune task.

---
+++ finetune_src/run/9-lxmert_p_5p_clip_pano_cls4nap_sratio_acc1_recbert4dec_3.5e.bash
@@ -0,0 +1,29 @@
+# name of the exp, where the "log.txt" and "weights" store
+name=${0}
+name=${name%.*}
+name=${name#*/}
+
+args="--gpu_id ${1}
+      --name ${name}
+      --train auglistener
+
+      --epoch 4
+      --batch_size 8
+      --optim adamW
+
+      --pretrain_path ../pretrain_src/snap/5-lxmert_p_5p_clip_pano_cls4nap_acc1/best_model_3/bert
+
+      --features clip_vit
+      --aug r2r_data/prevalent_aug.json
+      --speaker_aug 0
+
+      --decision_mode recbert
+      --x_layers 4
+      --feature_size 512
+      --angleFeatSize 128
+
+      --decay 0.0000001
+      --feedback sample
+      --mlWeight 0.20"
+
+nohup python -u r2r_src_simv1/train.py $args  > ${name}.log 2>&1 &
